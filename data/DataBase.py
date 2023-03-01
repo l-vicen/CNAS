6b9a0edfc2676a571724616fc19e5c72339a8775
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 10:44:24 +0100

    DEV: connection

--- data/DataBase.py
+++ data/DataBase.py
@@ -5,27 +5,28 @@ import pandas as pd
 from google.oauth2 import service_account
 from gsheetsdb import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
 def get_db():
 
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
     conn = connect(credentials=credentials)
 
-    @st.cache(ttl=600)
+    # Perform SQL query on the Google Sheet.
+    # Uses st.cache_data to only rerun when the query changes or after 10 min.
+    @st.cache_data(ttl=600)
     def run_query(query):
         rows = conn.execute(query, headers=1)
+        rows = rows.fetchall()
         return rows
 
     sheet_url = st.secrets["private_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-    tickers = []
+    # Print results.
     for row in rows:
-        tickers.append(f"{row.ticker}")
-
-    return tickers
+        st.write(f"{row.name} has a :{row.pet}:")
