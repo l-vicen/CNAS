a962734cbfa8e1b01230052c5048e481d24778e8
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 13:55:03 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -9,17 +9,16 @@ from gsheetsdb import connect
 import gspread
 
 # Create a connection object and config
-credentials = service_account.Credentials.from_service_account_info(
+CREDENTIALS = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
     ],
 )
-conn = connect(credentials=credentials) # Connection
-gc, client = gspread.authorize(credentials)
 
 # Get Request to own built DB -> Data containing <Wallet | Credit Score>
 def get_db():
+    gc = gspread.service_account_from_dict(CREDENTIALS)
     sh = gc.open("CNAS_DataSet")
     worksheet = sh.get_worksheet(1)
     df_read = get_as_dataframe(worksheet)
