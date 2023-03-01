bba7a5a24aa00f77322e0893f2b580b48d59d556
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 10:52:56 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -5,19 +5,20 @@ import pandas as pd
 from google.oauth2 import service_account
 from gspread_dataframe import get_as_dataframe
 from gsheetsdb import connect
+import gspread
 
-def get_db():
+# Create a connection object and config
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials) # Connection
 
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[
-            "https://www.googleapis.com/auth/spreadsheets",
-        ],
-    )
-    conn = connect(credentials=credentials)
-
-    sh = conn.open("CNAS_DataSet")
+def get_db():   
+    sa = gspread.service_account(st.secrets())
+    sh = sa.open("CNAS_DataSet")
     worksheet = sh.get_worksheet(1)
     df_read = get_as_dataframe(worksheet)
     st.write(df_read)
