4991f55703ce780050cd38308d45b783529c5b0a
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 13:30:54 +0100

    DEV: credentials

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import data.Extractor as Extractor
 import data.DataBase as DataBase
-import gspread
 
 # In-house dependencies
 st.title("ComprasNet's Auction Solver (CNAS)")
