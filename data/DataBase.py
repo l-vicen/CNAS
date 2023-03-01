029e4f47297a9386d93b474c1e53c84a32f82908
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 15:12:26 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -11,40 +11,26 @@ CREDENTIALS = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=["https://www.googleapis.com/auth/spreadsheets",],)
 
-CONNECTION = connect(":memory:", adapter_kwargs={
-    "gsheetsapi" : { 
-    "service_account_info" : {
-        "type" : st.secrets["gcp_service_account"]["type"],
-        "project_id" : st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key" : st.secrets["gcp_service_account"]["private_key"],
-        "client_email" : st.secrets["gcp_service_account"]["client_email"],
-        "client_id" : st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]
-        }
-    },
-})
-
-# def run_query(query):
-#     rows = connection.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     st.write(rows)
-#     return rows
-
-# def get_db():
-
-#     sheet_url = st.secrets["private_gsheets_url"]
-#     rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-#     # Print results.
-#     for row in rows:
-#         st.write(row)
-
 def post_db():
-    cursor = CONNECTION.cursor()
+
+    connection = connect(":memory:", adapter_kwargs={
+        "gsheetsapi" : { 
+        "service_account_info" : {
+            "type" : st.secrets["gcp_service_account"]["type"],
+            "project_id" : st.secrets["gcp_service_account"]["project_id"],
+            "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+            "private_key" : st.secrets["gcp_service_account"]["private_key"],
+            "client_email" : st.secrets["gcp_service_account"]["client_email"],
+            "client_id" : st.secrets["gcp_service_account"]["client_id"],
+            "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+            "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+            "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]
+            }
+        },
+    })
+
+    cursor = connection.cursor()
     sheet_url = st.secrets["private_gsheets_url"]
     # query = f'INSERT INTO "{sheet_url}" VALUES ("{name}", "{email}", "{q1a1}", "{q1a2}", "{q1a3}")'
     query = f'INSERT INTO "{sheet_url}" VALUES ("Testing 1", "Testing 1", "Testing 1","Testing 1", "Testing 1", "Testing 1","Testing 1", "Testing 1", "Testing 1", "Testing 1")'
