bb6c7864e37c064b41a0fb64ea29329311a0fb12
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 11:10:43 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -7,27 +7,28 @@ from gspread_dataframe import get_as_dataframe
 from gsheetsdb import connect
 import gspread
 
-# Create a connection object.
-CREDENTIALS = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-CONN = connect(credentials=CREDENTIALS)
+def get_db():
 
-def get_sheet_as_dataframe(CONN):
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    conn = connect(credentials=credentials)
 
     # Perform SQL query on the Google Sheet.
     # Uses st.cache_data to only rerun when the query changes or after 10 min.
     @st.cache_data(ttl=600)
     def run_query(query):
-        rows = CONN.execute(query, headers=1)
+        rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
         return rows
 
     sheet_url = st.secrets["private_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
     dataframe = pd.DataFrame(list(rows))
     st.write(dataframe)
     return dataframe
