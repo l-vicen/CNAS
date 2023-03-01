70181e134498864590343b4f23467dff3a58a90f
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 13:36:16 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -1,6 +1,7 @@
 # 3rdParty Libraries
 import streamlit as st
 import pandas as pd
+import re
 
 from google.oauth2 import service_account
 from gsheetsdb import connect
@@ -15,7 +16,7 @@ credentials = service_account.Credentials.from_service_account_info(
 conn = connect(credentials=credentials)
 
 def get_db(): 
-    sheet_id = st.secrets["private_gsheets_url"]
+    sheet_id = re.search('/d/(.+?)/edit?', st.secrets["private_gsheets_url"]).group(1)
     csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
     df =  pd.read_csv(csv_url, on_bad_lines='skip')
     st.write(df)
