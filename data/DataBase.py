da6b30f1c6c0b11929da693cc1a361d3d05ab462
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 15:06:12 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -4,29 +4,16 @@ import pandas as pd
 import re
 
 from google.oauth2 import service_account
+from gsheetsdb import connect
 from shillelagh.backends.apsw.db import connect
 
-# Create a connection object and config
-CREDENTIALS = service_account.Credentials.from_service_account_info(
+credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
-    scopes=["https://www.googleapis.com/auth/spreadsheets",],)
-
-connection = connect(":memory:", adapter_kwargs={
-    "gsheetsapi" : { 
-    "service_account_info" : {
-        "type" : st.secrets["gcp_service_account"]["type"],
-        "project_id" : st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key" : st.secrets["gcp_service_account"]["private_key"],
-        "client_email" : st.secrets["gcp_service_account"]["client_email"],
-        "client_id" : st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]
-        }
-    },
-})
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
 def run_query(query):
     rows = connection.execute(query, headers=1)
