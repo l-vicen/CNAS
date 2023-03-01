3ab1ec3f585a43715edfacb32db6c78fb5f982ca
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 15:08:17 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -4,36 +4,49 @@ import pandas as pd
 import re
 
 from google.oauth2 import service_account
-from gsheetsdb import connect
-# from shillelagh.backends.apsw.db import connect
+from shillelagh.backends.apsw.db import connect
 
-credentials = service_account.Credentials.from_service_account_info(
+# Create a connection object and config
+CREDENTIALS = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-connection = connect(credentials=credentials)
+    scopes=["https://www.googleapis.com/auth/spreadsheets",],)
 
-def run_query(query):
-    rows = connection.execute(query, headers=1)
-    rows = rows.fetchall()
-    st.write(rows)
-    return rows
+connection = connect(":memory:", adapter_kwargs={
+    "gsheetsapi" : { 
+    "service_account_info" : {
+        "type" : st.secrets["gcp_service_account"]["type"],
+        "project_id" : st.secrets["gcp_service_account"]["project_id"],
+        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+        "private_key" : st.secrets["gcp_service_account"]["private_key"],
+        "client_email" : st.secrets["gcp_service_account"]["client_email"],
+        "client_id" : st.secrets["gcp_service_account"]["client_id"],
+        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]
+        }
+    },
+})
 
-def get_db():
+# def run_query(query):
+#     rows = connection.execute(query, headers=1)
+#     rows = rows.fetchall()
+#     st.write(rows)
+#     return rows
 
-    sheet_url = st.secrets["private_gsheets_url"]
-    rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# def get_db():
 
-    # Print results.
-    for row in rows:
-        st.write(row)
-
-# def post_db():
-#     cursor = connection.cursor()
 #     sheet_url = st.secrets["private_gsheets_url"]
-#     # query = f'INSERT INTO "{sheet_url}" VALUES ("{name}", "{email}", "{q1a1}", "{q1a2}", "{q1a3}")'
-#     query = f'INSERT INTO "{sheet_url}" VALUES ("{2}", "{5}", "{6}", "{7}", "{8}")'
-#     cursor.execute(query)
+#     rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+#     # Print results.
+#     for row in rows:
+#         st.write(row)
+
+def post_db():
+    cursor = connection.cursor()
+    sheet_url = st.secrets["private_gsheets_url"]
+    # query = f'INSERT INTO "{sheet_url}" VALUES ("{name}", "{email}", "{q1a1}", "{q1a2}", "{q1a3}")'
+    query = f'INSERT INTO "{sheet_url}" VALUES ("{2}", "{5}", "{6}", "{7}", "{8}")'
+    cursor.execute(query)
 
