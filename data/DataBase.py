44d5a0cc766825111f1853d6a490eb82099549a3
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 11:45:12 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -2,10 +2,10 @@
 import streamlit as st
 import pandas as pd
 
-import gspread
-from gspread_dataframe import get_as_dataframe
 from google.oauth2 import service_account
+from gspread_dataframe import get_as_dataframe
 from gsheetsdb import connect
+import gspread
 
 # Create a connection object and config
 credentials = service_account.Credentials.from_service_account_info(
@@ -16,26 +16,11 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials) # Connection
 
-CONNECTION =  gspread.service_account("credentials.json")
-DB = CONNECTION.open("CNAS_DataSet")
-WORKSHEET = DB.get_worksheet(1)
 
 # Get Request to own built DB -> Data containing <Wallet | Credit Score>
 def get_db():
-    df_read = get_as_dataframe(WORKSHEET, usecols=[0,1], nrows=20)
-    st.write(df_read)
+    sa = gspread.service_account("CNAS/credentials.json")
+    sh = sa.open("CNAS_DataSet")
+    worksheet = sh.get_worksheet(1)
+    df_read = get_as_dataframe(worksheet)
     return df_read
-
-def post_db():
-    l = len(WORKSHEET.col_values(1))+1
-
-    WORKSHEET.update_cell(l, 1, "A1")
-    WORKSHEET.update_cell(l, 2,  "A2")
-    WORKSHEET.update_cell(l, 3, "A3")
-    WORKSHEET.update_cell(l, 4,  "A4")
-    WORKSHEET.update_cell(l, 5,  "A5")
-    WORKSHEET.update_cell(l, 6,  "A6")
-    WORKSHEET.update_cell(l, 7,  "A7")
-    WORKSHEET.update_cell(l, 8,  "A8")
-    WORKSHEET.update_cell(l, 9,  "A9")
-    WORKSHEET.update_cell(l, 10,  "A10")
