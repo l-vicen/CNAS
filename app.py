e41bd3e14fa97b49caf1ded5ed621abc252b52ee
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 10:38:50 +0100

    DEV: connection

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import data.DataBase as DataBase
 # In-house dependencies
 st.title("ComprasNet's Auction Solver (CNAS)")
 
-DataBase.get_sheet()
+DataBase.get_db()
 
 # Insert Target 
 text_input = st.text_input("Enter the auctionID: ", help= "The actionID is the identifier of the Pregao.")
