2e85d72baf4c4b9bd9e3d02045688611c84690fd
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-01 13:39:06 +0100

    DEV: credentials

--- data/DataBase.py
+++ data/DataBase.py
@@ -4,7 +4,7 @@ import pandas as pd
 import re
 
 from google.oauth2 import service_account
-from gsheetsdb import connect
+import gspread
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -14,11 +14,18 @@ credentials = service_account.Credentials.from_service_account_info(
     ],
 )
 conn = connect(credentials=credentials)
+client = gspread.authorize(credentials)
 
 def get_db(): 
     sheet_id = re.search('/d/(.+?)/edit?', st.secrets["private_gsheets_url"]).group(1)
-    st.write(sheet_id)
     csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
     df =  pd.read_csv(csv_url, on_bad_lines='skip')
     st.write(df)
     return df
+
+def post_db():
+    database_df = database_df.astype(str)
+    sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
+    sheet = client.open_by_url(sheet_url).sheet1
+    sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
+    st.success('Data has been written to Google Sheets')
