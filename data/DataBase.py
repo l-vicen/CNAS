90ea38fc4196a22d11a007e670708499f4f44ce9
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-02 14:20:21 +0100

    dev

--- data/DataBase.py
+++ data/DataBase.py
@@ -1,10 +1,10 @@
 # 3rdParty Libraries
 import streamlit as st
 import pandas as pd
-
 from google.oauth2 import service_account
 from shillelagh.backends.apsw.db import connect
 
+# Credential setting to access the private Google Sheet Data Set
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
@@ -12,10 +12,19 @@ credentials = service_account.Credentials.from_service_account_info(
     ],
 )
 
+"""
+
+Default way to access the private information on the credentials to access the Google Sheet Data Set.
+
+Via st.secrets["gcp_service_account"] I store private credential info on a file .streamlit/secrets.toml 
+that it is used on the hosting side (Streamlit Cloud) to establish communication between app and data set
+without disclosing critical information.
+
+"""
 connection = connect(":memory:", adapter_kwargs={
     "gsheetsapi" : { 
     "service_account_info" : {
-        "type" : st.secrets["gcp_service_account"]["type"],
+        "type" : st.secrets["gcp_service_account"]["type"], 
         "project_id" : st.secrets["gcp_service_account"]["project_id"],
         "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
         "private_key" : st.secrets["gcp_service_account"]["private_key"],
@@ -29,9 +38,18 @@ connection = connect(":memory:", adapter_kwargs={
     },
 })
 
+# Establishing the connection
 CURSOR = connection.cursor()
+
+# URL of private Google Sheet
 SHEET_URL = st.secrets["private_gsheets_url"]
 
+"""
+get_db() uses shillelagh lib to perform a common get
+request in SQL & Python. Here, I retrive all Tuples from the
+Google Sheet (A Tuple is a single row in the data set). Then,
+I convert the tuples to a dataframe.
+"""
 def get_db():
     query = f'SELECT * FROM "{SHEET_URL}"'
     rows = CURSOR.execute(query)
@@ -42,6 +60,15 @@ def get_db():
                                   'History_Bids_Items', 'History_Bids_Date'])
     return df
 
+"""
+post_db() uses shillelagh lib to perform a "writing" operation to 
+the data set. It inserts, the target data from ComprasNet into the 
+Google Sheet. 
+
+One row in the Google Sheet represents one Auction lot, one Auction 
+can have multiple lots.
+
+"""
 def post_db():
     # query = f'INSERT INTO "{sheet_url}" VALUES ("{name}", "{email}", "{q1a1}", "{q1a2}", "{q1a3}")'
     query = f'INSERT INTO "{SHEET_URL}" VALUES ("{"Testing 1"}", "{"Testing 1"}", "{"Testing 1"}","{"Testing 1"}", "{"Testing 1"}", "{"Testing 1"}","{"Testing 1"}", "{"Testing 1"}", "{"Testing 1"}", "{"Testing 1"}")'
--- data/Extractor.py
+++ data/Extractor.py
@@ -3,43 +3,57 @@ import pandas as pd
 import streamlit as st
 import requests
 import re
-import time
-import json
 
-""" Global Variables Definitions
-
-In this section, I define the macro variables to build the strings.
-
-(1) Query Auction Summary for specific Auction
-    - URL: "https://compras.dados.gov.br/pregoes/doc/pregao/`<AuctionID>`.csv"
-    - `AuctionID`: 1530740900092006
-
-(2) Query Auction Items for specific Auction
-    - URL: "https://compras.dados.gov.br/pregoes/id/pregao/`<AuctionID>`/itens.csv"
-    - `AuctionID`: 1530740900092006
-
-(3) Query Item specific list of bids for a specific Auction
-    - "https://compras.dados.gov.br/pregoes/v1/proposta_item_pregao.csv?item=`<ItemID>`&co_pregao=`<coPregaoID>`"
-    https://compras.dados.gov.br/pregoes/v1/proposta_item_pregao.json?item=709095&co_pregao=29107
-    - `<ItemID>`: 709103
-    - `<coPregaoID>`: 29107
-"""
+# Global Variables used to build http request targets
 URL_MAIN =  "https://compras.dados.gov.br"
 URL_AUCTION = "/pregoes/doc/pregao/"
 JSON_TYPE = ".json"
 
+"""
+
+get_auction_summary(auctionId) does http request
+to target: https://compras.dados.gov.br/pregoes/doc/pregao/<auctionID>json
+and return a .json object with summary data from one auction.
+
+param auctionID Auction Identifier 
+"""
 def get_auction_summary(auctionID):
     target = URL_MAIN + URL_AUCTION + auctionID + JSON_TYPE
     responseAuctionSummary = requests.get(url = target)
     auction_summary_json_dictionary = responseAuctionSummary.json()
-    return auction_summary_json_dictionary
 
+    return {
+        "co_processo": auction_summary_json_dictionary["co_processo"],
+        "co_uasg": auction_summary_json_dictionary["co_uasg"],
+        "dtDataEdital": auction_summary_json_dictionary["dtDataEdital"],
+        "dtInicioProposta": auction_summary_json_dictionary["dtInicioProposta"],
+        "dtFimProposta": auction_summary_json_dictionary["dtFimProposta"],
+        "valorHomologadoTotal": auction_summary_json_dictionary["valorHomologadoTotal"],
+        "valorEstimadoTotal": auction_summary_json_dictionary["valorEstimadoTotal"]
+    }
+
+"""
+
+get_auction_itens_information(auction_summary_json_dictionary) does http request
+to target: <TODO>
+and return a .json object with the information on the items auctioned in a specific auction. 
+
+param auction_summary_json_dictionary <TODO>
+"""
 def get_auction_itens_information(auction_summary_json_dictionary):
     targetItems = URL_MAIN + auction_summary_json_dictionary["_links"]["itens"]["href"] + JSON_TYPE
     responseItems = requests.get(url = targetItems)
     auction_items_json_dictionary = responseItems.json()
     return auction_items_json_dictionary
 
+"""
+
+get_items_bid_history_for_auction(auction_items_json_dictionary) does http request
+to target: <TODO>
+and return a .json object with the history of bids for every item auctioned.
+
+param auction_summary_json_dictionary <TODO>
+"""
 def get_items_bid_history_for_auction(auction_items_json_dictionary):
     pregoes_list = auction_items_json_dictionary["_embedded"]["pregoes"]
     list_target = [(URL_MAIN + re.sub(r"(.html)", r".json", pregoes_list[i]["_links"]["Propostas"]["href"])) for i in range(len(pregoes_list))]
