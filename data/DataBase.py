29a9e069aabce1f6f2a762bd3a1a2960a8da0c27
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-03 15:49:25 +0100

    DEV: test

--- data/DataBase.py
+++ data/DataBase.py
@@ -112,7 +112,7 @@ def post_db(auction_id, auction_summary, auction_items, auction_history):
     Participating_Suppliers = [auction_history[i]["nu_cpfcnpj_fornecedor"] for i in range(length_auction_history)]
     st.write(Participating_Suppliers)
 
-    item_date_history_2Dlist = [[float(i) for i in j if (Participating_Suppliers[j] == auction_history[j]["nu_cpfcnpj_fornecedor"])] for j in auction_history["vl_global"]]
+    item_date_history_2Dlist = [float(i) for i in range(length_auction_history) if (Participating_Suppliers[i] == auction_history[i]["nu_cpfcnpj_fornecedor"])]
     st.write(item_date_history_2Dlist)
 
     query = f'INSERT INTO "{SHEET_URL}" VALUES ("{auction_id}",\
