1d16ec7336f220b61742c5997e8d9d5d0fd2e390
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-03 16:34:18 +0100

    DEV: test

--- data/DataBase.py
+++ data/DataBase.py
@@ -104,9 +104,7 @@ def post_db(auction_id, auction_summary, auction_items, auction_history):
 
     """ 2nd Part: Getting data from auction_items query """
     
-    st.write(auction_history[0]["_embedded"]["pregoes"])
-
-
+    st.write(auction_history[0])
 
     # Variable to avoid re-accessing the inside of the auction_history json multiple times
     # auction_history = auction_history["_embedded"]["pregoes"]
@@ -116,11 +114,11 @@ def post_db(auction_id, auction_summary, auction_items, auction_history):
     # Participating_Suppliers = [auction_history[i]["nu_cpfcnpj_fornecedor"] for i in range(length_auction_history)]
     # number_supplier = len(Participating_Suppliers)
 
-    # # Creates a list with the history bids of the respective supplier
-    # item_bid_history_2Dlist = [[auction_history[i]["vl_global"] for i in range(length_auction_history) if (auction_history[i]["nu_cpfcnpj_fornecedor"] == Participating_Suppliers[j])] for j in range(number_supplier)]
+    # Creates a list with the history bids of the respective supplier
+    item_bid_history_2Dlist = [[auction_history[i]["vl_global"] for i in range(length_auction_history) if (auction_history[i]["nu_cpfcnpj_fornecedor"] == Participating_Suppliers[j])] for j in range(number_supplier)]
 
-    # # Creates a list with the history dates of the bids of the respective supplier
-    # item_date_history_2Dlist = [[auction_history[i]["dtRegistro"] for i in range(length_auction_history) if (auction_history[i]["nu_cpfcnpj_fornecedor"] == Participating_Suppliers[j])] for j in range(number_supplier)]
+    # Creates a list with the history dates of the bids of the respective supplier
+    item_date_history_2Dlist = [[auction_history[i]["dtRegistro"] for i in range(length_auction_history) if (auction_history[i]["nu_cpfcnpj_fornecedor"] == Participating_Suppliers[j])] for j in range(number_supplier)]
 
     # smallest_bid = auction_history[0]["vl_menorlance"]
 
