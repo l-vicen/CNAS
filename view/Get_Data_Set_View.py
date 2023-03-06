2803bfab7f3ec45f5459d9d519ece1d95b63edb8
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 15:20:23 +0100

    DEV: packages

--- view/Get_Data_Set_View.py
+++ view/Get_Data_Set_View.py
@@ -4,4 +4,4 @@ import data.DataBase as DataBase
 def get_data_set_view():
     st.title("GET: Data Set")
     st.info("This page displays the ComprasNet's data set constructed during the thesis period.")
-    st.write(DataBase.get_db())
+    # st.write(DataBase.get_db())
