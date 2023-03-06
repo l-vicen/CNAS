6d2079ce48e5da0990467ae6cd0445e64ce4e879
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 11:33:38 +0100

    DEV: test

--- view/Get_Data_Set_View.py
+++ view/Get_Data_Set_View.py
@@ -4,4 +4,4 @@ import data.DataBase as DataBase
 def construct_data_set_page():
     st.title("GET: Data Set")
     st.info("This page displays the ComprasNet's data set constructed during the thesis period.")
-    DataBase.get_db()
+    st.write(DataBase.get_db())
