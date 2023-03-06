ea2ba4f16a038b33bf71e1a95fe678a8ed1c68af
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 15:57:27 +0100

    DEV: packages

--- app.py
+++ app.py
@@ -17,11 +17,9 @@ page = st.sidebar.selectbox('Choose Page', features)
 st.sidebar.markdown('---')
 
 if page == features[0]:
-    # get_view.get_data_set_view()
-    pass
+    get_view.get_data_set_view()
 elif page == features[1]:
-    # post_view.insert_into_data_set_view()
-    pass
+    post_view.insert_into_data_set_view()
 else:
     auction_solver_view.solve_auction()
 
