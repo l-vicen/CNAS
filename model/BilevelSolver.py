178f2d42e31792483d8e74ac723d0f92f9730ceb
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 15:22:47 +0100

    DEV: packages

--- model/BilevelSolver.py
+++ model/BilevelSolver.py
@@ -111,43 +111,8 @@ def build_model():
     # Visualizing model composition with results
     # model.pprint()
 
-    st_stdout(model.pprint())    
+    model.pprint() 
 
     # model.display()
     # solver.write()
     # solver.pptrint()
-
-
-@contextmanager
-def st_redirect(src, dst):
-    placeholder = st.empty()
-    output_func = getattr(placeholder, dst)
-
-    with StringIO() as buffer:
-        old_write = src.write
-
-        def new_write(b):
-            if getattr(current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME, None):
-                buffer.write(b + '')
-                output_func(buffer.getvalue() + '')
-            else:
-                old_write(b)
-
-        try:
-            src.write = new_write
-            yield
-        finally:
-            src.write = old_write
-
-@contextmanager
-def st_stdout(dst):
-    "this will show the prints"
-    with st_redirect(sys.stdout, dst):
-        yield
-
-
-@contextmanager
-def st_stderr(dst):
-    "This will show the logging"
-    with st_redirect(sys.stderr, dst):
-        yield
