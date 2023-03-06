d051768bed777a9b8bfdc0382190a6a022f9c37e
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 11:27:58 +0100

    DEV: test

--- app.py
+++ app.py
@@ -1,58 +1,26 @@
 # 3rd party library dependencies
 import streamlit as st
-import data.Extractor as Extractor
-import data.DataBase as DataBase
-import view.DataSet_View as ds_view
+import view.Get_Data_Set_View as get_view
+import view.Post_Data_Set_View as post_view
 
-# Home page title
-st.title("ComprasNet's Auction Solver (CNAS)")
-
-"""Side Bar Class Declaration""" 
-
+# Side Bar Class Declaration
 st.sidebar.image('assets/tum_logo.png')
 st.sidebar.markdown('---')
 
 # Sidebar attributes
-features = ['Home Page', 'Solve Auction', 'Data Set']
+features = ['Get Data', 'Insert Data', 'Solve Auction']
 page = st.sidebar.selectbox('Choose Page', features)
 st.sidebar.markdown('---')
 
 if page == features[0]:
-    pass
+    get_view.construct_data_set_page()
 
 elif page == features[1]:
-    pass
+    post_view.insert_into_data_set_view()
 
 else:
-    ds_view.construct_data_set_page()
+    pass
 
 st.sidebar.markdown('##### Creator')
 st.sidebar.markdown('Lucas Perasolo')
 st.sidebar.markdown('---')
-
-
-# Insert Target 
-text_input = st.text_input("Enter the auctionID: ", help= "The actionID is the identifier of the Pregao.")
-
-items_bid_history = []
-
-if(text_input):
-
-    auction_summary_data = Extractor.get_auction_summary(text_input)
-    # st.markdown("## Auction Summary")
-    # st.write(auction_summary_data)
-    # st.markdown("---")
-
-    items_auctioned = Extractor.get_auction_itens_information(text_input)
-    # st.markdown("## Items Auctioned")
-    # st.write(items_auctioned)
-    # st.markdown("---")
-
-    items_bid_history = Extractor.get_items_bid_history_for_auction(items_auctioned)
-    # st.markdown("## Items Bid History")
-    # st.write(items_bid_history)
-    # st.markdown("---")
-
-
-if (len(items_bid_history) > 0):
-    DataBase.post_db(text_input, auction_summary_data, items_auctioned, items_bid_history)
---
+++ view/Post_Data_Set_View.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import data.Extractor as Extractor
+import data.DataBase as DataBase
+
+def insert_into_data_set_view():
+
+    # Insert Target 
+    text_input = st.text_input("Enter the auctionID: ", help= "The actionID is the identifier of the Pregao.")
+
+    items_bid_history = []
+
+    if(text_input):
+
+        auction_summary_data = Extractor.get_auction_summary(text_input)
+        # st.markdown("## Auction Summary")
+        # st.write(auction_summary_data)
+        # st.markdown("---")
+
+        items_auctioned = Extractor.get_auction_itens_information(text_input)
+        # st.markdown("## Items Auctioned")
+        # st.write(items_auctioned)
+        # st.markdown("---")
+
+        items_bid_history = Extractor.get_items_bid_history_for_auction(items_auctioned)
+        # st.markdown("## Items Bid History")
+        # st.write(items_bid_history)
+        # st.markdown("---")
+
+
+    if (len(items_bid_history) > 0):
+        DataBase.post_db(text_input, auction_summary_data, items_auctioned, items_bid_history)
