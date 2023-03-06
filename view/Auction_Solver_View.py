a7d01dff34a5e025d6d385dd3bcbcaf8c4086b7b
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-06 11:38:57 +0100

    DEV: test

---
+++ view/Auction_Solver_View.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def solve_auction():
+    st.title("Solver: Applying Bilevel Programming")
--- view/Get_Data_Set_View.py
+++ view/Get_Data_Set_View.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import data.DataBase as DataBase
 
-def construct_data_set_page():
+def get_data_set_view():
     st.title("GET: Data Set")
     st.info("This page displays the ComprasNet's data set constructed during the thesis period.")
     st.write(DataBase.get_db())
