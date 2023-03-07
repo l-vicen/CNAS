71740a2fd93a0b6b66d71fc9f364f6b9e8765af4
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 14:57:11 +0100

    DEV: Test

--- view/Auction_Solver_View.py
+++ view/Auction_Solver_View.py
@@ -1,13 +1,14 @@
 import streamlit as st
-import model.BilevelSolver as bls
+import model.Bilevel_Programming as bls
+import model.Solver_Parser as sl
 import data.DataBase as db
-import model.Parser as ps
+
 
 def solve_auction():
     st.title("SOLVER: Bilevel Programming")
 
     dataframe = db.get_db()
 
-    st.write(ps.get_items_auctioned(1530740900092006, dataframe))
+    st.write(sl.get_items_auctioned("1530740900092006", dataframe))
 
     bls.build_model()
