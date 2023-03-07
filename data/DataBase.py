48e63504c2717a11365403520c61e52a9436784a
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 10:51:58 +0100

    DEV: Connection

--- data/DataBase.py
+++ data/DataBase.py
@@ -5,12 +5,12 @@ from google.oauth2 import service_account
 from shillelagh.backends.apsw.db import connect
 
 # Credential setting to access the private Google Sheet Data Set
-# credentials = service_account.Credentials.from_service_account_info(
-#     st.secrets["gcp_service_account"],
-#     scopes=[
-#         "https://www.googleapis.com/auth/spreadsheets",
-#     ],
-# )
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
 
 """
 
@@ -21,27 +21,25 @@ that it is used on the hosting side (Streamlit Cloud) to establish communication
 without disclosing critical information.
 
 """
-# CONNECTION = connect(":memory:", adapter_kwargs={
-#     "gsheetsapi" : { 
-#     "service_account_info" : {
-#         "type" : st.secrets["gcp_service_account"]["type"], 
-#         "project_id" : st.secrets["gcp_service_account"]["project_id"],
-#         "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
-#         "private_key" : st.secrets["gcp_service_account"]["private_key"],
-#         "client_email" : st.secrets["gcp_service_account"]["client_email"],
-#         "client_id" : st.secrets["gcp_service_account"]["client_id"],
-#         "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
-#         "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
-#         "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-#         "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
-#         }
-#     },
-# })
-
-CONNECTION = connection = connect(":memory:")
+CONNECTION = connect(":memory:", adapter_kwargs={
+    "gsheetsapi" : { 
+    "service_account_info" : {
+        "type" : st.secrets["gcp_service_account"]["type"], 
+        "project_id" : st.secrets["gcp_service_account"]["project_id"],
+        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+        "private_key" : st.secrets["gcp_service_account"]["private_key"],
+        "client_email" : st.secrets["gcp_service_account"]["client_email"],
+        "client_id" : st.secrets["gcp_service_account"]["client_id"],
+        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+        }
+    },
+})
 
 # URL of private Google Sheet
-SHEET_URL = st.secrets["public_gsheets_url"]
+SHEET_URL = st.secrets["private_gsheets_url"]
 
 # Name of the columns in the google sheet
 GOOGLE_SHEET_COLUMNS = ['Auction_Id', 
