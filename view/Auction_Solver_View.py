0260c957bfc417cd1b37d3c46d1a4dbff2de21b6
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 14:48:51 +0100

    DEV: Test

--- view/Auction_Solver_View.py
+++ view/Auction_Solver_View.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import model.BilevelSolver as bls
+import data.DataBase as db
 
 def solve_auction():
     st.title("SOLVER: Bilevel Programming")
+
+    df = db.get_db()
+    st.dataframe(df)
+
+
     bls.build_model()
