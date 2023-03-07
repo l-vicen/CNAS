34bca5276c868b2836d89eb0d3adc29ae5969654
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 15:25:37 +0100

    DEV: test mssg

--- model/Bilevel_Programming.py
+++ model/Bilevel_Programming.py
@@ -114,7 +114,7 @@ def build_model():
     solver = Solver('pao.pyomo.FA')
     results = solver.solve(model)   
 
-    with st_stdout("warning"):
+    with st_stdout("info"):
         model.display()
 
     # Display built model in streamlit
