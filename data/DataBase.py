08bb68a9a6024755627480abe78436af80dc13a6
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 11:47:21 +0100

    DEV: test

--- data/DataBase.py
+++ data/DataBase.py
@@ -7,15 +7,13 @@ from shillelagh.backends.apsw.db import connect
 
 def establish_connection():
 
-    # Credential setting to access the private Google Sheet Data Set
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[
-            "https://www.googleapis.com/auth/spreadsheets",
-        ],
-    )
-
-    connection = connect(credentials=credentials)
+    # # Credential setting to access the private Google Sheet Data Set
+    # credentials = service_account.Credentials.from_service_account_info(
+    #     st.secrets["gcp_service_account"],
+    #     scopes=[
+    #         "https://www.googleapis.com/auth/spreadsheets",
+    #     ],
+    # )
 
     """
 
@@ -26,22 +24,22 @@ def establish_connection():
     without disclosing critical information.
 
     """
-    # connection = connect(":memory:", adapter_kwargs={
-    #     "gsheetsapi" : { 
-    #     "service_account_info" : {
-    #         "type" : st.secrets["gcp_service_account"]["type"], 
-    #         "project_id" : st.secrets["gcp_service_account"]["project_id"],
-    #         "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
-    #         "private_key" : st.secrets["gcp_service_account"]["private_key"],
-    #         "client_email" : st.secrets["gcp_service_account"]["client_email"],
-    #         "client_id" : st.secrets["gcp_service_account"]["client_id"],
-    #         "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
-    #         "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
-    #         "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-    #         "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
-    #         }
-    #     },
-    # })
+    connection = connect(":memory:", adapter_kwargs={
+        "gsheetsapi" : { 
+        "service_account_info" : {
+            "type" : st.secrets["gcp_service_account"]["type"], 
+            "project_id" : st.secrets["gcp_service_account"]["project_id"],
+            "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+            "private_key" : st.secrets["gcp_service_account"]["private_key"],
+            "client_email" : st.secrets["gcp_service_account"]["client_email"],
+            "client_id" : st.secrets["gcp_service_account"]["client_id"],
+            "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+            "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+            "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+            }
+        },
+    })
 
     return connection
 
