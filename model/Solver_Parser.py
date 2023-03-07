17f241e124a81a40b44926e773bfe074dfaa0a83
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 16:26:07 +0100

    DEV: Test

--- model/Solver_Parser.py
+++ model/Solver_Parser.py
@@ -8,3 +8,11 @@ def get_participating_suppliers(auction_id, dataframe):
 
 def get_demand_items_auctioned(auction_id, dataframe):
     return dataframe.loc[dataframe["Auction_Id"] == auction_id, "Demanded_Quantity_Items"][0]
+
+""" Parser helper method to construct dictionary """
+def parse_to_dictionary_format(auctioned_items, numeric_value_list):
+    return dict(zip(auctioned_items, numeric_value_list))
+
+""" Parser helper method to construct a list of tuples from two lists"""
+def parse_two_lists_into_one_tuple_list(list_one, list_two):
+    return list(zip(list_one, list_two))
--- view/Auction_Solver_View.py
+++ view/Auction_Solver_View.py
@@ -2,6 +2,7 @@ import streamlit as st
 import model.Bilevel_Programming as bls
 import model.Solver_Parser as sl
 import data.DataBase as db
+import re
 
 def solve_auction():
     st.title("SOLVER: Bilevel Programming")
@@ -17,6 +18,11 @@ def solve_auction():
 
     if (btn_clicked):
 
+        # TODO: Dictionary of Item Utility {Item, Demand}
+        # TODO: Dictionary of Item Expected Cost {Item, ExpectedCost}
+        # TODO: LIST of Participating Suppliers
+        # TODO: Dictionary {Tuple, Integer} of Supplier Capacity 
+
         # Getting list of auctioned items
         list_auction_items = sl.get_items_auctioned(text_input, dataframe)
         st.write(list_auction_items)
@@ -25,7 +31,26 @@ def solve_auction():
         list_demand_items = sl.get_demand_items_auctioned(text_input, dataframe)
         st.write(list_demand_items)
 
+        # Building DICT: {Item, Demand}
+        demand = sl.parse_to_dictionary_format(list_auction_items, list_demand_items)
+        st.write(demand)
+
+        # Building DICT: {Item, Demand}
+        utility_input = get_utility_from_user()
+        utility = sl.parse_to_dictionary_format(list_auction_items, utility_input)
+        st.write(utility)
+
         st.write({'Lot_Item': 'RODO', 'Participating_Suppliers': ['41205907000174', '06910908000119'], 'History_Bids_Lot': [['1804'], ['4800']], 'History_Bid_Dates_Lot': [['2006-06-07T00:00:00'], ['2006-06-08T00:00:00']], 'Winning_Bid': 1804.0, 'Winner_Supplier': None})
-        
+
         # Build model & Solve
         bls.build_model()
+
+def get_utility_from_user():
+
+    collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
+    numbers = st.text_input("PLease enter numbers")
+    st.write(collect_numbers(numbers))
+
+    fixed_numbers = st.multiselect("Please select numbers", [1, 2, 3, 4, 5])
+    st.write(fixed_numbers)
+    return fixed_numbers
