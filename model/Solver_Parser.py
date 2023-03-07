0d675410e3dc3eddd41ae30da4a202687fbf130e
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 18:39:56 +0100

    DEV: test

--- model/Solver_Parser.py
+++ model/Solver_Parser.py
@@ -15,9 +15,7 @@ def get_cell_as_list(auction_id, dataframe, column):
     return list_target if (column == "Items_Auctioned") else [float(i) for i in list_target]
 
 def get_lot_summary_as_list_of_dictionaries(auction_id, dataframe):
-    string_items = dataframe.loc[dataframe["Auction_Id"] == auction_id, "Auction_Lot_Summary"][0]
-    string_items = string_items.lstrip()
-    string_items = string_items.rstrip()
+    string_items = dataframe.loc[dataframe["Auction_Id"] == auction_id, "Auction_Lot_Summary"][0].replace("[{", "{").replace("}]", "}")
     string_items = string_items.replace(", {'Lot_Item':", "**&** {'Lot_Item':")
     return string_items.split("**&** ")
 
