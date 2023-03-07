500ecc24b9207c845f44ae8d2adf90e421e83f5a
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 16:48:39 +0100

    DEV: auctions

--- model/Solver_Parser.py
+++ model/Solver_Parser.py
@@ -3,7 +3,7 @@ import re
 
 def get_items_auctioned(auction_id, dataframe, column):
     string_items = dataframe.loc[dataframe["Auction_Id"] == auction_id, column][0]
-    string_items = string_items.replace("\'",'').replace("]","").replace("[","")
+    string_items = string_items.replace("\'",'').replace("]","").replace("[","").lstrip()
     list_string_items = string_items.split(',')
     return list_string_items if column == "Items_Auctioned" else [int(i) for i in list_string_items]
 
