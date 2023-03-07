4e26f121ef20c9f997fc24fa0801fad42fdb4d2d
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 12:09:33 +0100

    Clear

--- view/Post_Data_Set_View.py
+++ view/Post_Data_Set_View.py
@@ -8,19 +8,20 @@ def insert_into_data_set_view():
 
     # Insert Target 
     text_input = st.text_input("Enter the auctionID: ", key="auction_id", help= "The actionID is the identifier of the Pregao.")
-    
+    auction_id = text_input
+
     btn_clicked = st.button("Insert", on_click=reset_auction_id)
 
     items_bid_history = []
 
     if(btn_clicked):
 
-        auction_summary_data = Extractor.get_auction_summary(text_input)
+        auction_summary_data = Extractor.get_auction_summary(auction_id)
         # st.markdown("## Auction Summary")
         # st.write(auction_summary_data)
         # st.markdown("---")
 
-        items_auctioned = Extractor.get_auction_itens_information(text_input)
+        items_auctioned = Extractor.get_auction_itens_information(auction_id)
         # st.markdown("## Items Auctioned")
         # st.write(items_auctioned)
         # st.markdown("---")
@@ -32,7 +33,8 @@ def insert_into_data_set_view():
 
 
     if (len(items_bid_history) > 0):
-        DataBase.post_db(text_input, auction_summary_data, items_auctioned, items_bid_history)
+        DataBase.post_db(auction_id, auction_summary_data, items_auctioned, items_bid_history)
+        items_bid_history = []
 
 def reset_auction_id():
     st.session_state["auction_id"] = ""
