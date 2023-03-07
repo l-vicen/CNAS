11c78534278803ae21327c74c50b7ebba92f11a8
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 09:47:24 +0100

    Test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # 3rd party library dependencies
 import streamlit as st
-# import view.Get_Data_Set_View as get_view
-# import view.Post_Data_Set_View as post_view
+import view.Get_Data_Set_View as get_view
+import view.Post_Data_Set_View as post_view
 import view.Auction_Solver_View as auction_solver_view
 
 # App global config setting
@@ -17,11 +17,9 @@ page = st.sidebar.selectbox('Choose Page', features)
 st.sidebar.markdown('---')
 
 if page == features[0]:
-    # get_view.get_data_set_view()
-    pass
+    get_view.get_data_set_view()
 elif page == features[1]:
-    # post_view.insert_into_data_set_view()
-    pass
+    post_view.insert_into_data_set_view()
 else:
     auction_solver_view.solve_auction()
 
