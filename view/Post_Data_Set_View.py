71a2396f17468a8e88be39b3055cbbe4cc3bd2cb
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-07 12:07:39 +0100

    DEV: insert multiple

--- view/Post_Data_Set_View.py
+++ view/Post_Data_Set_View.py
@@ -7,11 +7,13 @@ def insert_into_data_set_view():
     st.title("POST: Data Set")
 
     # Insert Target 
-    text_input = st.text_input("Enter the auctionID: ", help= "The actionID is the identifier of the Pregao.")
+    text_input = st.text_input("Enter the auctionID: ", key="auction_id", help= "The actionID is the identifier of the Pregao.")
+    
+    btn_clicked = st.button("Insert", on_click=reset_auction_id)
 
     items_bid_history = []
 
-    if(text_input):
+    if(btn_clicked):
 
         auction_summary_data = Extractor.get_auction_summary(text_input)
         # st.markdown("## Auction Summary")
@@ -31,3 +33,6 @@ def insert_into_data_set_view():
 
     if (len(items_bid_history) > 0):
         DataBase.post_db(text_input, auction_summary_data, items_auctioned, items_bid_history)
+
+def reset_auction_id():
+    st.session_state["auction_id"] = ""
