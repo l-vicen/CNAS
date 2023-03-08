66541e55c1147b74d7bcae48fbfc93b5a9947661
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-08 18:28:08 +0100

    DEV: test

--- model/Bilevel_Programming.py
+++ model/Bilevel_Programming.py
@@ -88,8 +88,11 @@ def build_model(set_items, set_suppliers, demand_dictionary, utility_dictionary,
     print_into_streamlit("Suppliers\' individual Production Cost per Item",  model.L.Production_Costs)
 
     # Objective function assignments
-    model.o = Objective(rule=auction_objective_function(model), sense=maximize, doc='Auction Problem') # Upper-level 
-    model.L.o = Objective(rule= pricing_objective_function(model.L, model), sense=maximize, doc='Pricing Problem') # Lower-level
+    model.o = Objective(rule=auction_objective_function(model), sense=maximize, doc='Auction_Problem') # Upper-level 
+    print_into_streamlit("Upper-level Objective Function",  model.o)
+
+    model.L.o = Objective(rule= pricing_objective_function(model.L, model), sense=maximize, doc='Pricing_Problem') # Lower-level
+    print_into_streamlit("Lower-level Objective Function",  model.L.o )
 
     # Upper-level constraint assignments
     model.SingleSourcingConstraint = Constraint(model.i, rule=single_sourcing_constraint, doc='There is at most 1 winner')
