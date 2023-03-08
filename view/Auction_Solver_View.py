6889026d6c2ca5db7fa10e44c3636a98384fbbf8
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-08 14:26:28 +0100

    DEV: test

--- view/Auction_Solver_View.py
+++ view/Auction_Solver_View.py
@@ -84,19 +84,16 @@ def solve_auction():
         # bls.build_model()
 
    # Building DICT: {Item, Utility}
-    st.markdown("#### Utility per Item")
     collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
     utility_input_list  = st.text_input("Please enter numbers")
-    st.write(collect_numbers(utility_input_list))
-
+    collect_numbers(utility_input_list)
     Utility = sl.parse_to_dictionary_format(list_auction_items, utility_input_list)
+    st.markdown("#### Utility per Item")
     st.write(Utility)
 
     # Building DICT: {Item, Utility}
-    st.markdown("#### Production Cost per Item")
-    collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
     cost_input_list  = st.text_input("Please enter numbers")
-    st.write(collect_numbers(cost_input_list))
-
+    collect_numbers(cost_input_list)
     Production_Cost = sl.parse_to_dictionary_format(list_auction_items, cost_input_list)
+    st.markdown("#### Production Cost per Item")
     st.write(Production_Cost)
