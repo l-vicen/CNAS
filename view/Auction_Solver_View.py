bb07749cc44aa1d70b352360a024abe2f1c8afc9
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-09 09:36:09 +0100

    DEV: utility

--- view/Auction_Solver_View.py
+++ view/Auction_Solver_View.py
@@ -4,6 +4,7 @@ import model.Solver_Parser as sl
 import data.DataBase as db
 from collections import OrderedDict
 import re
+import random
 
 def solve_auction():
 
@@ -114,20 +115,31 @@ def solve_auction():
             # #     st.write(key)
             # #     st.write(value)
             # st.markdown("---") 
+
+
+            # building Bilevel Program
+            number_auctioned_items = len(list_auction_items)
+            Utility = [random.uniform(100, 200) for i in range(number_auctioned_items)]
+
+            btn_apply_bilevel = st.button("Apply Bilevel Solver")
+            st.markdown("---")
+            if (btn_apply_bilevel):
+                bls.build_model(list_auction_items, Participating_Supplier, Demand, Utility, Suppliers_Capacity, Budget, Suppliers_Production_Cost)
+
             
             # Getting User Input
-            collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
-            utility_input_list  = st.text_input("Please enter numbers", key = "Utility_Input")
-            utility_list = collect_numbers(utility_input_list)
+            # collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
+            # utility_input_list  = st.text_input("Please enter numbers", key = "Utility_Input")
+            # utility_list = collect_numbers(utility_input_list)
 
-            if (utility_input_list):
-                Utility = sl.parse_to_dictionary_format(list_auction_items, utility_list)
+            # if (utility_input_list):
+            #     Utility = sl.parse_to_dictionary_format(list_auction_items, utility_list)
                 # st.markdown("#### Auctioneer's perceived Utility per Item")
                 # st.write(Utility)
                 # st.markdown("---")
 
-                # building Bilevel Program
-                btn_apply_bilevel = st.button("Apply Bilevel Solver")
-                st.markdown("---")
-                if (btn_apply_bilevel):
-                    bls.build_model(list_auction_items, Participating_Supplier, Demand, Utility, Suppliers_Capacity, Budget, Suppliers_Production_Cost)
+                # # building Bilevel Program
+                # btn_apply_bilevel = st.button("Apply Bilevel Solver")
+                # st.markdown("---")
+                # if (btn_apply_bilevel):
+                #     bls.build_model(list_auction_items, Participating_Supplier, Demand, Utility, Suppliers_Capacity, Budget, Suppliers_Production_Cost)
