ff249dd375bde0146eadc1a94d8a4d2428d041f9
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-09 20:02:32 +0100

    Pause: on length empty suppls

--- model/Solver_Parser.py
+++ model/Solver_Parser.py
@@ -1,11 +1,9 @@
 import ast
 
 """ Returns the cell value as a list in a targeted column based on the auction id."""
-def get_cell_as_list(auction_id, dataframe, column, dtype):
+def get_cell_as_list(auction_id, dataframe, column):
     cell_value = dataframe.loc[dataframe["Auction_Id"] == auction_id, column].iloc[0]
-    cell_value = " ".join(cell_value.split())
-    cell_values_as_list = ast.literal_eval(cell_value)  
-    return [x.lower() for x in cell_values_as_list] if dtype == 1 else cell_values_as_list
+    return ast.literal_eval(cell_value)
 
 """ Returns the cell value as a list of dictionaries in the column {"Auction_Lot_Summary"} based on the auction id."""
 def get_cell_as_list_of_dict(auction_id, dataframe):
@@ -13,11 +11,12 @@ def get_cell_as_list_of_dict(auction_id, dataframe):
     cell_value = cell_value.replace("[{", "{")
     cell_value = cell_value.replace("}]", "}")
     cell_value = cell_value.replace(", {", "**&** {")
-    list_auction_lots = [ast.literal_eval(x) for x in cell_value.split("**&** ")]
-    for auction_lot in list_auction_lots:
-        auction_lot["Lot_Item"] = " ".join(auction_lot["Lot_Item"].split()).lower()
-    return list_auction_lots
+    return [ast.literal_eval(x) for x in cell_value.split("**&** ")]
 
 """ Parser helper method to construct dictionary """
 def parse_to_dictionary_format(auctioned_items, numeric_value_list):
-    return dict(zip([str(x) for x in auctioned_items], numeric_value_list))
+    return dict(zip(auctioned_items, numeric_value_list))
+
+""" Parser helper method to construct a list of tuples from two lists"""
+def parse_two_lists_into_one_tuple_list(list_one, list_two):
+    return list(zip(list_one, list_two))
