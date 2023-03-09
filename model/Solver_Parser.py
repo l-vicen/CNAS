948c8bdcb27988723d7ba4b66016a7418d3637aa
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-09 18:38:34 +0100

    DEV: types

--- model/Solver_Parser.py
+++ model/Solver_Parser.py
@@ -2,7 +2,7 @@ import ast
 
 """ Returns the cell value as a list in a targeted column based on the auction id."""
 def get_cell_as_list(auction_id, dataframe, column):
-    cell_value = dataframe.loc[dataframe["Auction_Id"] == auction_id, column].iloc[0]
+    cell_value = dataframe.loc[dataframe["Auction_Id"] == auction_id, column].iloc[0].lower()
     return ast.literal_eval(cell_value)
 
 """ Returns the cell value as a list of dictionaries in the column {"Auction_Lot_Summary"} based on the auction id."""
