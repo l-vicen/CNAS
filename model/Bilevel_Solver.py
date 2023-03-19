f5baaf7dcdfdb93079384882f50c3e8f8397e643
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-19 18:36:52 +0100

    example

--- model/Bilevel_Solver.py
+++ model/Bilevel_Solver.py
@@ -145,10 +145,12 @@ def priceVector_plot(list_items, actual_winning_bids_list, estimated_prices_list
 
     dataframe = pd.DataFrame(list(zip(list_items, total_expected_expense_price, total_actual_winning_bid_price, total_price_model_suggestion)), columns=['Items', 'Expected Pricing', 'Actual Winning Pricing', 'Model Suggested Pricing'])
     
-    st.write(dataframe)
+    df = px.data.tips()
+    st.write(df)
+    fig = px.box(df, x="day", y="total_bill", color="smoker")
+    # st.write(dataframe)
 
-    # Here we use a column with categorical data
-    fig = px.box(dataframe)
+    # fig = px.box(dataframe)
     st.plotly_chart(fig, use_container_width=True)
 
 def auctionWinners_HeatMap(winner_dataframe):
