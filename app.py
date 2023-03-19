f93621b2a29fec67030413680a2fd32b1ea4eb15
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-19 10:26:35 +0100

    Rename

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # 3rd party library dependencies
 import streamlit as st
-import view.Get_Data_Set_View as get_view
-import view.Post_Data_Set_View as post_view
-import view.Auction_Solver_View as auction_solver_view
+import view.Get_Data_View as get_view
+import view.Post_Data_View as post_view
+import view.Solver_View as solver_view
 
 # App global config setting
 st.set_page_config(layout="wide")
@@ -23,7 +23,7 @@ elif page == features[1]:
 elif page == features[2]:
     post_view.insert_into_data_set_view()
 else:
-    auction_solver_view.solve_auction()
+    solver_view.solve_auction()
 
 st.sidebar.markdown('##### Creator')
 st.sidebar.markdown('Lucas Perasolo')
