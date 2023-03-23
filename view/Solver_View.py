9075d3680d78bb47f3813c0d72401e5190752dd4
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-23 11:15:12 +0100

    DEV: reference

--- view/Solver_View.py
+++ view/Solver_View.py
@@ -103,7 +103,7 @@ def solve_auction():
             chosen_solver = st.selectbox('How would you like to solve the bilevel problem?', solver_options)
             
             if ('pao.pyomo.MIBS'):
-                st.warning("The iterface to MibS is a prototype that has not been well-tested. This interface will be documented and finalized in an upcoming release of PAO. For more see (https://pao.readthedocs.io/en/latest/solvers.html).")
+                st.warning("\"The iterface to MibS is a prototype that has not been well-tested. This interface will be documented and finalized in an upcoming release of PAO\". For more see (https://pao.readthedocs.io/en/latest/solvers.html).")
 
             # Executing Bilevel Solver if btn is pressed
             btn_apply_bilevel = st.button("Apply Bilevel Solver")
