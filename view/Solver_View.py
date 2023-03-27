645a4617fe85e263e511a8ca84741e2c175d8d84
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-27 20:18:27 +0200

    DEV: M

--- view/Solver_View.py
+++ view/Solver_View.py
@@ -101,7 +101,7 @@ def solve_auction():
 
             solver_options = ['pao.pyomo.FA','pao.pyomo.MIBS']
             chosen_solver = st.selectbox('How would you like to solve the bilevel problem?', solver_options)
-            bigM = st.number_input('Choose Big-M Value', key=bigM,value=100)
+            bigM = st.number_input('Choose Big-M Value', value=100)
         
             if (chosen_solver == 'pao.pyomo.MIBS'):
                  st.warning("\"The iterface to MibS is a prototype that has not been well-tested. This interface will be documented and finalized in an upcoming release of PAO\". For more see (https://pao.readthedocs.io/en/latest/solvers.html).")
@@ -109,8 +109,8 @@ def solve_auction():
             # Executing Bilevel Solver if btn is pressed
             btn_apply_bilevel = st.button("Apply Bilevel Solver")
             st.markdown("---")
+            
             if (btn_apply_bilevel):
-                
                 # For Visualizations
                 Estimated_prices_list = sl.get_cell_as_list(text_input, dataframe, "Estimated_Price_Items")
                 Actual_winning_bids_list = sl.get_cell_as_list(text_input, dataframe, "Winning_Bids")
