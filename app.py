e00cc41906790816b1abe7e3d36a2b404db08e89
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-03-31 19:02:28 +0200

    INSERT Video

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ if page == features[0]:
     st.info("Is is a web application able to query auction data from ComprasNet Platform storing them into a proprietary data base (Google Sheet).")
     st.markdown("---")
     st.markdown("## How to use it")
+    st.markdown("#### Insert Feature Tutorial")
+    insert_vid = open('assets/videos/insert.mp4', 'rb')
+    insert = insert_vid.read()
+    st.video(insert)
+    st.markdown("---")
+    st.markdown("#### Bilevel Solver Tutorial")
+    # video_file_prepProp = open('assets/videos/preprocessing.mp4', 'rb')
+    st.markdown("---")
 elif page == features[1]:
     get_view.get_data_set_view()
 elif page == features[2]:
